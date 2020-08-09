75ef9475c9c37fb27d58331cc8859b259846687e
Abrahamcepe <abrahamgolf@gmail.com>
2020-08-09 09:53:43 -0500

    Deleted initial react page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const firebase = requere('firebase');
+const firebase = require('firebase');
 require('firebase/firestore');
 
 firebase.initializeApp({
