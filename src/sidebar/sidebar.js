dee945802e3066b15ef4a28b5a66cbdae087c182
Abrahamcepe <abrahamgolf@gmail.com>
2020-08-10 09:03:48 -0500

    Added more code

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -21,25 +21,56 @@ class SidebarComponent extends React.Component{
             <div className={classes.sidebarContainer}>
                 <Button
                 onClick={this.newNoteBtnClick}
-                className={classes.newNoteBtn}>New Note</Button>
+                className={classes.newNoteBtn}>{this.state.addingNote ? 'Cancel': 'New note'}</Button>
                 {
                     this.state.addingNote ? 
                     <div>
                         <input type="text"
-                        className={classes.newNoteInput}
-                        placeholder="Enter note title"
-                        onKeyUp={(e) => this.updateTitle(e.target.value)}></input>
+                            className={classes.newNoteInput}
+                            placeholder="Enter note title"
+                            onKeyUp={(e) => this.updateTitle(e.target.value)}>
+                        </input>
+                        <Button
+                            className={classes.newNoteSumbitBtn}
+                            onClick={this.newNote}>Submit Note</Button>
                     </div>:
                     null
                 }
+                <List>
+                    {
+                        notes.map((_note, _index) =>  {
+                            return (
+                                <div key={_index}>
+                                    <SidebarComponent
+                                        note={_note}
+                                        index={_index}
+                                        selectedNoteIndex={selectedNoteIndex}
+                                        selectNote={this.selectNote}
+                                        deleteNote={this.deleteNote}>
+                                    </SidebarComponent>
+                                    <Divider></Divider>
+                                </div>
+                            );
+                        })
+                    }
+                </List>
             </div>
         );
     }
     newNoteBtnClick = () => {
-        console.log("New btn clicked");
+        this.setState({ title: null, addingNote: !this.state.addingNote });
     }
     updateTitle = (txt) => {
-        console.log("Here it is: ", txt);
+        this.setState({ title: txt });
+    }
+    newNote =  () => {
+        console.log(this.state);
+    }
+    selectNote = () => {
+        console.log("select note")
+    }
+    deleteNote = () => {
+        console.log("delete note")
     }
 }
 
