d109342d3a15cc169ff4e53b348d6c65bacf58d3
Abrahamcepe <abrahamgolf@gmail.com>
2020-08-24 09:05:35 -0500

    Added select and delete functions to sideBarItemComponent

--- src/sidebarItem/sidebarItem.js
+++ src/sidebarItem/sidebarItem.js
@@ -24,9 +24,17 @@ class SidebarItemComponent extends React.Component {
                                     primary={_note.title}
                                     secondary={removeHTMLTags(_note.body.substring(0,30) + '...')}></ListItemText>
                         </div>
+                        <DeleteIcon onClick={() => this.deleteNote(_note)}
+                            className={classes.deleteIcon}></DeleteIcon>
                 </ListItem>
             </div>
         );
+    }
+    selectNote = (n, i) => this.props.selectNote(n, i);
+    deleteNote = (note) => {
+        if(window.confirm(`Are you sure you want to delete: ${note.title}`)){
+            this.props.deleteNote(note);
+        }
     }
 
 }
